8a1f9e3cdf512d0f126c6c6acf30b3b2645d7776
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-06 18:24:12 +0200

    commentato codice superfluo

--- js/main.js
+++ js/main.js
@@ -11,19 +11,19 @@ for (let boxNumber = 1; boxNumber <= boxValue; boxNumber++){
     const modulo3 = (boxNumber % 3);
 
     if ((modulo3 == 0) && (modulo5 == 0)){
-        console.log(boxNumber + " DIVISIBILE per 3 e 5");
+        // console.log(boxNumber + " DIVISIBILE per 3 e 5");
         messaggio = "fizzbuzz";
         stile = "color-gold"
     }
 
     else if (modulo5 == 0){
-        console.log(boxNumber + " divisibile per 5")
+        // console.log(boxNumber + " divisibile per 5")
         messaggio = "buzz";
         stile = "color-red";
     }
 
     else if (modulo3 == 0){
-        console.log(boxNumber + " divisibile per 3")
+        // console.log(boxNumber + " divisibile per 3")
         messaggio = "fizz";
         stile = "color-green"
     }
