0b3c6f74435d677145e9cafedfab6c0fe234472b
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-06 18:21:37 +0200

    aggiunto stile variabile divisibilità e prompt richiesta numero

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 const containerBox = document.getElementById("container");
 
+const boxValue = parseInt(prompt("Inseisci un numero!"));
 
-for (let boxNumber = 1; boxNumber <= 100; boxNumber++){
-    containerBox.innerHTML += '<div id="box">' + boxNumber + '</div>';
+for (let boxNumber = 1; boxNumber <= boxValue; boxNumber++){
 
-    const box = document.getElementById("box")
+    let messaggio = boxNumber;
+    let stile = "";
 
     const modulo5 = (boxNumber % 5);
     const modulo3 = (boxNumber % 3);
 
     if ((modulo3 == 0) && (modulo5 == 0)){
         console.log(boxNumber + " DIVISIBILE per 3 e 5");
+        messaggio = "fizzbuzz";
+        stile = "color-gold"
     }
 
     else if (modulo5 == 0){
         console.log(boxNumber + " divisibile per 5")
+        messaggio = "buzz";
+        stile = "color-red";
     }
 
     else if (modulo3 == 0){
         console.log(boxNumber + " divisibile per 3")
+        messaggio = "fizz";
+        stile = "color-green"
     }
 
-    else{
-        console.log(boxNumber + " NON divisibile per 3 e 5")
-    }
-
+    containerBox.innerHTML += '<div class="boxes ' + stile + '">' + messaggio + '</div>';
 }
