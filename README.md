61dd819df7dbfc939851b83f26b704585933a383
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-07-06 16:44:30 +0200

    aggiunto codice js determinazione divisibilità

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # js-fizzbuzz
+
+creo un container
+
+inizializzo una variabile per il container
+
+inizializzo una variabile con un ciclo con un incremento di numeri da 1 a 100
+
+inserisco la variabile all'interno di box e li stampo
+
+tramite l'utilizzo dell'operatore modulo calcolo la divisibilità di ogni boxNumber 
+
+se sono divisibili per 3 applico determinate classi
+
+se sono divisibili per 5 applico altre classi
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 const containerBox = document.getElementById("container");
 
+
 for (let boxNumber = 1; boxNumber <= 100; boxNumber++){
-    containerBox.innerHTML += '<div class="box">' + boxNumber + '</div>'
+    containerBox.innerHTML += '<div id="box">' + boxNumber + '</div>';
+
+    const box = document.getElementById("box")
+
+    const modulo5 = (boxNumber % 5);
+    const modulo3 = (boxNumber % 3);
+
+    if ((modulo3 == 0) && (modulo5 == 0)){
+        console.log(boxNumber + " DIVISIBILE per 3 e 5");
+    }
+
+    else if (modulo5 == 0){
+        console.log(boxNumber + " divisibile per 5")
+    }
+
+    else if (modulo3 == 0){
+        console.log(boxNumber + " divisibile per 3")
+    }
+
+    else{
+        console.log(boxNumber + " NON divisibile per 3 e 5")
+    }
+
 }
